e4fcb54ed2c6666a1715d7cc42a332ffedfeefae
sydney <sydney.idundun@gmail.com>
2022-09-09 14:00:26 +0200

    started working on the UI for the simple todo application

--- todo/src/index.jsx
+++ todo/src/index.jsx
@@ -3,6 +3,6 @@ import { render } from 'solid-js/web';
 
 import './index.css';
 import App from './App';
-import ShowTodo from './showTodo';
+import ShowTodo from './ShowTodo';
 
 render(() => <ShowTodo />, document.getElementById('root'));
--- todo/src/showTodo.jsx
+++ todo/src/showTodo.jsx
@@ -1,8 +1,13 @@
+import styles from './ShowTodo.module.css';
+
 function ShowTodo(){
     return (
-        <div >
-            <button>Todos</button>
-            <button>Completed</button>
+        <div class={styles.container}>
+            <div class={styles.tabHolder}>
+                <button class={styles.tab}>Todos</button>
+                <button class={styles.tab}>Completed</button>
+            </div>
+
             <div >
                 There are currently no todos present at this time
             </div>
